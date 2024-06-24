ef3b7d65b3544c14e820d15ce70e3d36a0a30a2e
eduardofc <eduardofernandezcarrion@gmail.com>
2024-06-24 06:59:09 +0200

    make predictions

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ from datetime import datetime
 from jobs.job_model_training import ModelTraining
 from jobs.job_make_predictions import MakePredictions
 from utils import read_yaml
-from model.model import Model
-
-
-def train_model(spark, ):
-
-    model = Model(spark)
-    model.read_daily_events()
-    model.train_test_split()
-    model.model_and_productivize()
-    model.save_train_test_metric_tables()
-    model.make_predictions()
 
 
 if __name__ == "__main__":
@@ -34,36 +23,16 @@ if __name__ == "__main__":
     """ 1 de febrero >> first-execution """
 
     # job model-training
-    job = ModelTraining(spark=spark, config=config, exec_time=exec_time)
-    job.run()
+    # job = ModelTraining(spark=spark, config=config, exec_time=exec_time)
+    # job.run()
 
     # job make-predictions
-    job = MakePredictions(spark=spark, config=config, exec_time=exec_time)
-    job.run()
+    # job = MakePredictions(spark=spark, config=config, exec_time=exec_time, date="2020-02-01")
+    # job.run()
+
+    """ 2 de febrero >> daily-execution """
+
+    # job evaluate-drift
 
 
-    table_daily_events = config['table']['daily_events']
 
-
-    # a) partimos de un dataset de enero ya esté en bbdd
-    # clientes = [f"{np.random.randint(10000, 99999)}{chr(np.random.randint(65, 90))}" for _ in range(500)]
-    # df_enero = carga_los_eventos_de_enero(spark, clientes, table_name=table_daily_events)
-
-    # b) entrenamos un modelo
-    train_model(spark)
-
-
-    # c) hacemos las predicciones para el 1 de febrero (y_hat)
-
-    """ 1 de febrero """
-    # a) nos dan los valores reales del 1 de febrero (y)
-
-    # b) estudiamos si hay drift (calculamos la metrica con y y_hat)
-    # if new_accuracy < accuracy_model_del_registry - 0.2
-
-    # c) si hay drift reentramos,
-
-    # d) hacemos las predicciones para el 2 de febrero
-
-    """ 2 de febrero """
-    # ...
